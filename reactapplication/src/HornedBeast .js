6ac04d58b6c3b5fea4dd691fc2cf7623ac73e45c
sajaalraja <sajamajed611@yahoo.com>
2021-06-22 15:13:11 +0300

    updatedlab2

--- reactapplication/src/HornedBeast .js
+++ reactapplication/src/HornedBeast .js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 
 class HornedBeast extends Component {
-    render() {
-        return (
-            <div>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.image_url}></img>
-                <p>{this.props.description}</p>
-            </div>
-        )
+    constructor(props) {
+        super(props)
+        this.state = {
+            vote: props.vote
+        }
     }
+
+votinggchange = () => {
+    this.setState({ vote: this.state.vote + 1 })
 }
 
+
+render(){
+
+
+    return (
+        <div>
+            <h2>{this.props.title}</h2>
+            <img src={this.props.imgage}></img>
+            <p>{this.props.description}</p>
+            <button onClick={this.votinggchange}>ask for voting</button>
+            <span> {this.state.vote}</span>
+        </div>
+    )
+
+}
+}
 export default HornedBeast;
